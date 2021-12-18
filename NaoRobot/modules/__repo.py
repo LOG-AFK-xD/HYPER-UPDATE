1b1eb7f07298143601af6c42cb4b98c0f8428216
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-18 15:17:58 +0530

    Update __repo.py

--- NaoRobot/modules/__repo.py
+++ NaoRobot/modules/__repo.py
@@ -2,8 +2,8 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import *
 
-from TGNRobot.conf import get_str_key
-from TGNRobot import pbot
+from NaoRobot.conf import get_str_key
+from NaoRobot import pbot
 
 REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/cab6825dea9263d347831.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Akki](t.me/godfatherakki) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @The_Godfather_Network «««"
   
