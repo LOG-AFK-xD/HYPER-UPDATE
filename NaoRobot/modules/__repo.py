f996295ea687ff3f39d00304dcda2a803dc5c1a3
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-18 16:39:16 +0530

    Update __repo.py

--- NaoRobot/modules/__repo.py
+++ NaoRobot/modules/__repo.py
@@ -2,21 +2,21 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import *
 
-from NaoRobot.conf import get_str_key
-from NaoRobot import pbot
+from TGNRobot.conf import get_str_key
+from TGNRobot import pbot
 
 REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/f819b0e13c279ff09e69b.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [𝗛𝗮𝗿𝘀𝗵](t.me/Harsh_Pandit) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @Harsh_Pandit_XD «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("💫Rᴇᴘᴏꜱɪᴛᴏʀʏ🔥", url=f"https://me/Log_Afk"),
-        InlineKeyboardButton("✨Cʜᴀᴛ Gʀᴏᴜᴘ💫", url=f"https://t.me/LOVE_X_POISON"),
+        InlineKeyboardButton("💫Rᴇᴘᴏꜱɪᴛᴏʀʏ🔥", url=f"https://t.me/Proud_of_indian"),
+        InlineKeyboardButton("✨Cʜᴀᴛ Gʀᴏᴜᴘ💫", url=f"https://t.me/Love_X_Poison"),
       ],[
-        InlineKeyboardButton("✌️ Oᴡɴᴇʀ ❣️", url="https://t.me/PROUD_OF_INDIAN"),
+        InlineKeyboardButton("✌️ Oᴡɴᴇʀ ❣️", url="https://t.me/LOG_AFK"),
         InlineKeyboardButton("✨Sᴜᴘᴘᴏʀᴛ 🔐", url="https://t.me/UNIQUE_SOCIETY"),
       ],[
-        InlineKeyboardButton("❣️ Bʜᴀɪ ☑️", url="https://t.me/Yash_Thakur_9"),
-        InlineKeyboardButton("❣️ Bʜᴀɪ ➡️", url="https://t.me/EVIL_BOY_XT"),
+        InlineKeyboardButton("❣️ Bʜᴀɪ ☑️", url="https://t.me/yash_thakur_9"),
+        InlineKeyboardButton("❣️ Bʜᴀɪ ☑️", url="https://t.me/Evil_boy_xt"),
       ]]
     )
   
