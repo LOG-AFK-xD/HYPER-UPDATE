640872841ef155be9e3e86ee603f4ce1baddd8cb
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-20 11:36:57 +0530

    Update hello.py

--- NaoRobot/modules/hello.py
+++ NaoRobot/modules/hello.py
@@ -7,11 +7,14 @@ from pyrogram import __version__ as pyrover
 from NaoRobot.events import register as MEMEK
 from NaoRobot import telethn as tbot
 
+PHOTO = "https://telegra.ph/file/674bb34bc80e2a6aaa0b6.jpg"
 
-@MEMEK(pattern=("/Hello"))
+@MEMEK(pattern=("/hello"))
 async def awake(event):
   tai = event.sender.first_name
   NAO = f"**╔┓┏╦━╦┓╔┓╔━━╗
 ║┗┛║┗╣┃║┃║X X║
 ║┏┓║┏╣┗╣┗╣╰╯║
 ╚┛┗╩━╩━╩━╩━━╝**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Akiramanagerbot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/AkiraSupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=NAO,  buttons=BUTTON)
