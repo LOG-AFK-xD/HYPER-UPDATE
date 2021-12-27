542ffc80079711a9140c83d7a22355952588ef6c
Harsh Pandit <93505687+HarshPanditXD@users.noreply.github.com>
2021-12-27 21:27:53 +0530

    Update alive.py

--- NaoRobot/modules/alive.py
+++ NaoRobot/modules/alive.py
@@ -7,16 +7,16 @@ from pyrogram import __version__ as pyrover
 from NaoRobot.events import register as MEMEK
 from NaoRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/3b6e36bcf650dd5ae53bf.png"
+PHOTO = "https://telegra.ph/file/70242b21dc297e8b9436b.jpg"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
   NAO = f"**Hello, I'm HYPERMEN ROBOT!** \n\n"
   NAO += "🔴 **I'm Working Properly** \n\n"
-  NAO += "🔴 **My Master : [Harsh](https://t.me/harsh_Pandit)** \n\n"
+  NAO += "🔴 **My Master : [𝐋𝐎𝐆 𝐀𝐅𝐊](https://t.me/Log_AFK)** \n\n"
   NAO += f"🔴 **Telethon Version : {tlhver}** \n\n"
   NAO += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
   NAO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Log_afk"), Button.url("Pᴏᴡᴇʀᴇᴅ Bʏ", "https://t.me/THE_FURIOUSNETWORK")]]
+  BUTTON = [[Button.url("👆Sᴜᴘᴘᴏʀᴛ✌️", "https://t.me/UNIQUE_SOCIETE"), Button.url("Pᴏᴡᴇʀᴇᴅ Bʏ", "https://t.me/LOG_AFK")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=NAO,  buttons=BUTTON)
